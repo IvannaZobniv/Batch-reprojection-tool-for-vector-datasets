981fcf7d0403e325c0f351a564b5be9ad0634fa3
Ivanna Zobniv <ivannazobniv333@gmail.com>
2021-04-18 19:03:31 +0300

    Ignore files build by Visual Studio *.lock

---
+++ Projected_target.py
@@ -0,0 +1,36 @@
+import arcpy
+import os
+
+#set up the paths
+targetFC = r"D:\MY\KNU\3_COURSE\2_Semertr\Programing_GIS\My_work\Arcpy_week_2\PE\Lesson2\Project.gdb\CountyLines"
+folderToExamine = r"D:\MY\KNU\3_COURSE\2_Semertr\Programing_GIS\My_work\Arcpy_week_2\PE\Lesson2\Project.gdb"
+arcpy.env.overwriteOutput = True
+
+#get spatial reference for the target feature class
+targetDescribe = arcpy.Describe(targetFC)
+targetSR = targetDescribe.SpatialReference
+targetSRName = targetSR.Name
+
+# Get a list of my feature classes
+arcpy.env.workspace = folderToExamine
+listOfFCs = arcpy.ListFeatureClasses()
+    #Loop through the list of FCs  
+for currentFC in listOfFCs:
+    print (currentFC)
+        #Read the spatial reference of the current one
+    currentFCDescribe = arcpy.Describe(currentFC)
+    currentFCSR = currentFCDescribe.SpatialReference
+    currentFCSRName = currentFCSR.Name
+    print (currentFCSRName)
+
+    if currentFCSRName != targetSRName:
+        print ("Spatial references don't match")
+    else:
+        print ("Spatial references do match")   
+    if currentFCSRName == targetSRName:
+    #skip
+        continue
+    else:
+    # Determine the new output feature class path and name
+        outCS = currentFC[:-4] +"_projected.shp"
+        arcpy.Project_management(currentFC, outCS, targetSR)
