2b20716a2031dad9c269b3c3522641a8a32660d1
Ivanna Zobniv <ivannazobniv333@gmail.com>
2021-04-28 05:58:01 +0300

    Created tools, completed tasks

--- Projected_target.py
+++ Projected_target.py
@@ -1,10 +1,10 @@
 import arcpy
 import os
 
-#set up the paths
-targetFC = r"D:\MY\KNU\3_COURSE\2_Semertr\Programing_GIS\My_work\Arcpy_week_2\PE\Lesson2\Project.gdb\CountyLines"
-folderToExamine = r"D:\MY\KNU\3_COURSE\2_Semertr\Programing_GIS\My_work\Arcpy_week_2\PE\Lesson2\Project.gdb"
 arcpy.env.overwriteOutput = True
+targetFC = arcpy.GetParameterAsText(0)
+folderToExamine = r"D:\MY\KNU\3_COURSE\2_Semertr\Programing_GIS\My_work\Arcpy_week_2\PE\Lesson2\Project.gdb"
+
 
 #get spatial reference for the target feature class
 targetDescribe = arcpy.Describe(targetFC)
@@ -34,3 +34,7 @@ for currentFC in listOfFCs:
     # Determine the new output feature class path and name
         outCS = currentFC[:-4] +"_projected.shp"
         arcpy.Project_management(currentFC, outCS, targetSR)
+        arcpy.AddMessage("All done!")
+else:
+    arcpy.AddMessage(arcpy.GetMessages())
+
